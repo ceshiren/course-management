3b289381be2fa4a0a6bf271feea4fdf47d2954eb
hogwarts_feier <fangyuan@ceshiren.com>
2022-05-07 22:02:32 +0800

    高校培训 jinjia

---
+++ jinjia_demo/data.py
@@ -0,0 +1,18 @@
+"""
+__author__ = '霍格沃兹测试开发学社'
+__desc__ = '更多测试开发技术探讨，请访问：https://ceshiren.com/t/topic/15860'
+"""
+courses = [
+    { "course_id": 1,
+      "course_name": "测开21期",
+      "course_desc": "python 高级课程"
+    },
+    { "course_id": 2,
+      "course_name": "测开22期",
+      "course_desc": "python 高级课程"
+    },
+    { "course_id": 3,
+      "course_name": "就业3期",
+      "course_desc": "python 基础课程"
+    }
+]
